cc7908ea1ec0f7c65811d010d6d34dc1f505c8fd
siamezzze <siamezzze@gmail.com>
2016-12-27 04:56:01 +0300

    Apply clustering to bring every digit to a fixed ammount of points.

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -3,7 +3,34 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 import os.path
+import random
+
+
+def is_corner(p0, p1, p2):
+    v1 = p1 - p0
+    v1 /= np.linalg.norm(v1)
+    v2 = p2 - p1
+    v2 /= np.linalg.norm(v2)
+    angle = math.acos(np.dot(v1, v2))
+    return (angle > math.pi / 2) and (angle < math.pi * 3 / 2)
+
+
+def number_of_corners(stroke):
+    px_0 = stroke[0, :]
+    px_1 = stroke[1, :]
+    corners = 0
+    for j in range(2, len(stroke)):
+        px = stroke[j, :]
+        step = np.linalg.norm(px - px_1)
+        if step < eps:
+            continue
+        corners += int(is_corner(px, px_1, px_0))
+        px_0 = px_1
+        px_1 = px
+    return corners
+
 
 strokes = {k: [] for k in range(10)}
 train_n = [84, 79, 78, 73, 76, 79, 84, 75, 70, 76]
@@ -25,7 +52,33 @@ for digit in range(10):
     for i in range(train_n[digit]):
         stroke = strokes[digit][i]
         stroke_2d = pca.transform(stroke)
+        mins = np.amin(stroke_2d, axis=0)
+        maxs = np.amax(stroke_2d, axis=0)
+        stroke_2d -= mins
         strokes_2d[digit].append(stroke_2d)
+
+        lengths = maxs - mins + 2
+        lengths = lengths.astype(int)
+        canvas = np.zeros(lengths, dtype=np.uint8)
+
+        for j in range(stroke_2d.shape[0]):
+            px = stroke_2d[j, :]
+            canvas[int(px[0]), int(px[1])] = 255
+
+        canvas_2d[digit].append(canvas)
+        # plt.imsave(os.path.join("vis", "digit_" + str(digit) + "_" + str(i + 1) + ".png"), canvas, cmap='Greys')
+
+eps = 1e-2
+for digit in range(10):
+    for i in range(len(strokes_2d[digit])):
+        # We have a problem: different amount of points in for different letters.
+        # Solve it by clustering down to a fixed amount.
+        corners = 0
+        stroke_2d = strokes_2d[digit][i]
+        kmeans = KMeans(n_clusters=16).fit(stroke_2d)
+        stroke_reduced = kmeans.cluster_centers_
+
+        # Visualisation
         mins = np.amin(stroke_2d, axis=0)
         maxs = np.amax(stroke_2d, axis=0)
 
@@ -33,9 +86,8 @@ for digit in range(10):
         lengths = lengths.astype(int)
         canvas = np.zeros(lengths, dtype=np.uint8)
 
-        for j in range(stroke_2d.shape[0]):
-            px = stroke_2d[j, :] - mins
+        for j in range(stroke_reduced.shape[0]):
+            px = stroke_reduced[j, :]
             canvas[int(px[0]), int(px[1])] = 255
 
-        canvas_2d[digit].append(canvas)
-        plt.imsave(os.path.join("vis", "digit_" + str(digit) + "_" + str(i + 1) + ".png"), canvas, cmap='Greys')
+        plt.imsave(os.path.join("kmeans", "digit_" + str(digit) + "_" + str(i + 1) + ".png"), canvas, cmap='Greys')

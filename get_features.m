6ae58ebefd8f174959d38d2673d113a164e22ca3
Maria Glukhova <siamezzze@gmail.com>
2016-12-30 06:46:25 +0200

    Visualisation

--- get_features.m
+++ get_features.m
@@ -7,8 +7,21 @@ function [ features ] = get_features( stroke_3d, pca_T, n_anchors)
     
     % 3D to 2D
     stroke_2d = stroke_3d * pca_T;
+    
+    % Visualisation
+    % scatter(stroke_2d(:, 1), stroke_2d(:, 2), 'b.');
+    % title('2D digit');
+    
     % To fixed ammount of points.
     [idx, centers] = kmeans(stroke_2d, n_anchors);
+    
+    %figure;
+    %scatter(centers(:, 1), centers(:, 2), 'b.');
+    %enmr = [1:n_anchors]'; enmr_str = num2str(enmr); 
+    %enmr_c = cellstr(enmr_str);
+    %d = 0.2;
+    %text(centers(:, 1) + d, centers(:, 2) + d, enmr_c);
+    %title('Centroids')
     
     % Restore ordering of anchor points.
     min_point = zeros(1, n_anchors);
@@ -20,8 +33,15 @@ function [ features ] = get_features( stroke_3d, pca_T, n_anchors)
     [~, argsort] = sort(min_point);
     anchors = centers(argsort, :);
     
+    %figure;
+    %scatter(anchors(:, 1), anchors(:, 2), 'b.');
+    %enmr = [1:n_anchors]'; enmr_str = num2str(enmr); 
+    %enmr_c = cellstr(enmr_str);
+    %d = 0.2;
+    %text(anchors(:, 1) + d, anchors(:, 2) + d, enmr_c);
+    %title('Centroids reordered');
+    
     % Collect features of anchor points.
-    % TODO: Try adding more features here.
     
     % Move origin to center of the image;
     maxs = max(anchors);
@@ -29,8 +49,9 @@ function [ features ] = get_features( stroke_3d, pca_T, n_anchors)
     lengths = maxs - mins;
     center = mins + lengths / 2;
     diag = sqrt(lengths(1) * lengths(1) + lengths(2) * lengths(2)) / 2;
-    maxtheta = pi;
     [theta, rho] = cart2pol(anchors(:, 1) - center(1), anchors(:, 2) - center(2));
+    % Normalize features for better KNN classification.
+    maxtheta = pi;
     rho = rho / diag;
     theta = theta / maxtheta;
     features = zeros(1, n_anchors * 2);
